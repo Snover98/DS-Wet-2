8ea49bd891c99115b46a06f3805e6ac5fe1dc813
Assaf-Haik-Barouch <Assaf98@gmail.com>
2017-11-28 10:53:42 +0200

    implemented comparitors by level and ID.

---
+++ CompGlads.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Assaf-Haik-Barouch on 28/11/2017.
+//
+
+#include "CompGlads.h"
+#include "Gladiator.h"
+/*****************************************
+* Compares the the inputted Gladiators by their IDs
+* return values:
+* if the ids are the same, 0
+* if g1.id < g2.id, -1
+* if g1.id > g2.id, 1
+*******************************************/
+int CompGladsByID::operator()(Gladiator& g1, Gladiator& g2) {
+    if(g1.getID() == g2.getID()){
+        return 0;
+    }
+
+    return (g1.getID() < g2.getID())? -1: 1;
+}
+
+/*****************************************
+* Compares the the inputted Gladiators by their Levels.
+* If they have the same level, they are compared by id IN REVERSE
+* return values:
+* if g1.level < g2.level, -1
+* if g1.level > g2.level, 1
+* if g1.level == g2.level and g1.id < g2.id, 1
+* if g1.level == g2.level and g1.id > g2.id, -1
+* if g1.level == g2.level and g1.id == g2.id, 0
+**************************************************/
+int CompGladsByLevel::operator()(Gladiator &g1, Gladiator& g2) {
+    if(g1.getLevel() == g2.getLevel()){
+        return (-1)*CompGladsByID::operator()(g1, g2);
+    }
+
+    return (g1.getLevel() < g2.getLevel())? -1: 1;
+}
---
+++ CompGlads.h
@@ -0,0 +1,48 @@
+//
+// Created by Assaf-Haik-Barouch on 28/11/2017.
+//
+
+#ifndef HW2_COMPGLADS_H
+#define HW2_COMPGLADS_H
+
+#include "Gladiator.h"
+
+
+
+virtual class CompGlads{
+    public:
+    virtual int operator()(Gladiator& g1, Gladiator& g2);
+};
+
+class CompGladsByID : public CompGlads{
+    public:
+    CompGladsByID() = default;
+    virtual int operator()(Gladiator& g1, Gladiator& g2) override;
+    /*****************COMPARISON************************
+     * Compares the the inputted Gladiators by their IDs
+     * return values:
+     * if the ids are the same, 0
+     * if g1.id < g2.id, -1
+     * if g1.id > g2.id, 1
+     **************************************************/
+};
+
+class CompGladsByLevel : public CompGladsByID{
+public:
+    CompGladsByLevel() = default;
+    int operator()(Gladiator& g1, Gladiator& g2) override;
+    /*****************COMPARISON************************
+     * Compares the the inputted Gladiators by their Levels.
+     * If they have the same level, they are compared by id IN REVERSE
+     * return values:
+     * if g1.level < g2.level, -1
+     * if g1.level > g2.level, 1
+     * if g1.level == g2.level and g1.id < g2.id, 1
+     * if g1.level == g2.level and g1.id > g2.id, -1
+     * if g1.level == g2.level and g1.id == g2.id, 0
+     **************************************************/
+};
+
+
+
+#endif //HW2_COMPGLADS_H
