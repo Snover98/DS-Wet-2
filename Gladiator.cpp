27f0727b14d3b8b78ba9f93f2c085b24fa2619dc
Assaf-Haik-Barouch <Assaf98@gmail.com>
2017-11-28 13:07:42 +0200

    implemented Gladiator class

---
+++ Gladiator.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Assaf-Haik-Barouch on 28/11/2017.
+//
+
+#include "Gladiator.h"
+
+Gladiator::Gladiator(int id, int lv, const Trainer &t): ID(id), level(lv), trainer(t) {}
+
+int Gladiator::getID() {
+    return this->ID;
+}
+
+int Gladiator::getLevel() {
+    return this->level;
+}
+
+const Trainer & Gladiator::getTrainer() {
+    return this->trainer;
+}
+
+void Gladiator::setID(int ID) {
+    this->ID = ID;
+}
+
+void Gladiator::setLevel(int level) {
+    this->level = level;
+}
