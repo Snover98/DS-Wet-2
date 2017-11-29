c0a1cba7eeff396a593ca3ae25331a52fd35832f
Oded <raoded@campus.technion.ac.il>
2017-11-29 15:56:13 +0200

    added splay tree file

---
+++ splayTree.h
@@ -0,0 +1,17 @@
+//
+// Created by oded on 29/11/17.
+//
+
+#ifndef DS_WET_2_SPLAYTREE_H
+#define DS_WET_2_SPLAYTREE_H
+
+template <class T, typename Compare>
+class SplayTree{
+
+private:
+    T& info
+public:
+
+};
+
+#endif //DS_WET_2_SPLAYTREE_H
