abb782b22338541b689cbec686c96e302240f952
Assaf-Haik-Barouch <Assaf98@gmail.com>
2017-12-03 17:39:51 +0200

    stripped remove of it's virtual status changed split parameters implemented find, findMax and findMin made empt functions for all other public functions

--- splayTree.h
+++ splayTree.h
@@ -38,13 +38,92 @@ public:
     void insert(T& info) override;
 
     //removes the node with relevant info. returns false if it doesn't exist, true otherwise.
-    virtual bool remove(T& info) override;
+    bool remove(T& info) override;
 
     //join two trees, where every node in tree2 has a higher info value than those in this tree
     void join(SplayTree tree2);
 
     //split the tree in accordance to the inserted pivot
-    SplayTree split();
+    SplayTree split(T& pivot);
 };
+
+template<class T, class Compare>
+T& SplayTree<T, Compare>::find(T& info){
+    //find the node or the closest one in value if it does not exist
+    TreeNode* found = BinTree::findNode(info);
+
+    //move the found node to the root
+    splay(found);
+
+    //if it has the correct info, return it
+    if(found->info == info){
+        return info;
+    }
+
+    //otherwise, it was not found so return NULL
+    return NULL;
+};
+
+template<class T, class Compare>
+T& SplayTree<T, Compare>::findMax() {
+    //find the max node
+    TreeNode* max_node = BinTree::findMax();
+
+    //move the max node to the root
+    splay(max_node);
+
+    //return the max info
+    return max_node->info;
+}
+
+template<class T, class Compare>
+T& SplayTree<T, Compare>::findMin() {
+    //find the min node
+    TreeNode* min_node = BinTree::findMin();
+
+    //move the min node to the root
+    splay(min_node);
+
+    //return the min info
+    return min_node->info;
+}
+
+template<class T, class Compare>
+void SplayTree<T, Compare>::insert(T &info) {
+
+
+
+
+
+}
+
+template<class T, class Compare>
+bool SplayTree<T, Compare>::remove(T &info){
+
+
+
+
+
+}
+
+template<class T, class Compare>
+void SplayTree<T, Compare>::join(SplayTree tree2){
+
+
+
+
+
+}
+
+template<class T, class Compare>
+SplayTree<T, Compare> SplayTree<T, Compare>::split(T &pivot){
+
+
+
+
+
+}
+
+
 
 #endif //DS_WET_2_SPLAYTREE_H
